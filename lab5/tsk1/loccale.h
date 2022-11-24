5253468829b61f8b77fde852cb37ac906f6e248f
fferikvalik <81646006+fferikvalik@users.noreply.github.com>
2022-11-24 10:56:09 +0300

    Create loccale.h

---
+++ lab5/tsk1/loccale.h
@@ -0,0 +1,19 @@
+//
+// Created by Valentin Gouriev on 18.11.22.
+//
+
+#ifndef TSK1_LOCCALE_H
+#define TSK1_LOCCALE_H
+#include <stdlib.h>
+#include <stdio.h>
+
+// print arr
+void inputn(int* n);
+int* memory(int n);
+void printarr(float* arr, int n);
+// Input array elements:
+void inputarr(float* arr, int n);
+// work in arr
+void workarr(float* arr, int n, int* count);
+int *mem(int n, float* arr);
+#endif //TSK1_LOCCALE_H
