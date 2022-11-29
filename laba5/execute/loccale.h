901801159f85696b39be1f337426d43eccb772c4
fferikvalik <81646006+fferikvalik@users.noreply.github.com>
2022-11-29 14:07:14 +0300

    Create loccale.h

---
+++ laba5/execute/loccale.h
@@ -0,0 +1,27 @@
+//
+// Created by Valentin Gouriev on 18.11.22.
+//
+
+#ifndef TSK1_LOCCALE_H
+#define TSK1_LOCCALE_H
+#include <stdlib.h>
+#include <stdio.h>
+
+// print arr
+
+void printArchLogo();
+void inputn1(int* n);
+int* memory1(int n);
+void printarr1(float* arr, int n);
+void inputarr1(float* arr, int n);
+void workarr1(float* arr, int n, int* count);
+int *mem1(int n, float* arr);
+void* freee2(int n, int**matr);
+void input_col2(int* m);
+void input_row2(int* n);
+int* memory2(int n, int m);
+void print_matr2(int** matr, int n,int m);
+void work_in_matr3(int** matr, int n, int m);
+void work_in_matr2(int** matr, int n, int m);
+int choose_input2(int **matr, int n,int m);
+#endif //TSK1_LOCCALE_H
